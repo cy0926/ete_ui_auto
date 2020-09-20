1cf4be9fb86db38d9cf06bbeb45cc0c780cae75c
yukin <yukincao@etekcity.com.cn>
2020-09-20 12:56:56 +0800

    上传

--- config/getDriver.py
+++ config/getDriver.py
@@ -8,6 +8,7 @@ def get_driver():
     option = webdriver.ChromeOptions()
     option.add_argument("--window-size=1920,1080")
     option.add_argument("--start-maximized")
+    option.add_argument("--no-sandbox")
     option.add_argument("--headless")  # 浏览器不提供可视化页面
     option.add_argument('--log-level=3')
     driver = webdriver.Chrome(chrome_options=option)
